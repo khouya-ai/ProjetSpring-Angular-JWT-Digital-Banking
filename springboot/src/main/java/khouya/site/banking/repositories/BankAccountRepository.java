e51b2ac875e64b81638314821471fa21ed0841cb
Oussama khouya <oussama.khouya-etu@etu.univh2c.ma>
2025-05-18 19:24:53 +0100

    list accounts by customer

--- springboot/src/main/java/khouya/site/banking/repositories/BankAccountRepository.java
+++ springboot/src/main/java/khouya/site/banking/repositories/BankAccountRepository.java
@@ -3,5 +3,8 @@ package khouya.site.banking.repositories;
 import khouya.site.banking.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+    List<BankAccount> getBankAccountByCustomer_Id(Long customerId);
 } 
